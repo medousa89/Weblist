13f0692b82e6a8427247011174475f4b973fcec1
medousa89 <medousa89@gmail.com>
2021-05-25 16:46:02 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget "https://raw.githubusercontent.com/free-greek-iptv/greek-iptv/master/Greekstreamtv.m3u"
 sed -e "/#EXTM3U/d" -e "s/.*,/Channel name:/" -e "s/^http/URL1:http/" -e "s/https/http/g"  "Greekstreamtv.m3u" > "WebTV List.txt"
+rv -rf Greekstreamtv.m3u
