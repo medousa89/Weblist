3385ec62dca4ac3eb6d2504fea12c72f1182ca51
medousa89 <medousa89@gmail.com>
2023-07-10 15:20:07 +0300

    Update new.py

--- new.py
+++ new.py
@@ -3,7 +3,6 @@ from os import path, listdir
 
 GSXML = "stream.xml"
 GSBQ = "userbouquet.greekstreamtv.tv"
-GSBQ2 = "userbouquet.greekstreamtvATV.tv"
 
 try:
             from xml.etree.cElementTree import ElementTree
@@ -26,24 +25,5 @@ try:
                     f.write(service)
 except Exception as err:
             print("[GreekStreamTV] Exception: %s" % err)
-try:
-            from xml.etree.cElementTree import ElementTree
-            tree = ElementTree()
-            tree.parse(GSXML)
-            tvlist = []
-            for iptv in tree.findall("iptv"):
-                name = iptv.findtext("name").title()
-                (protocol, serviceType, bufferSize, epgId) = iptv.findtext("type").split(":")
-                uri = iptv.findtext("uri")
-                if protocol in "livestreamer":
-                    uri = "http://localhost:8088/" + uri
-                uri = uri.replace(":", "%3a")
-                service = "#SERVICE {s}:0:1:{e}:{e}:0:0:0:0:0:{u}:{n}\n".format(s=serviceType, e=epgId, u=uri, n=name)
-                tvlist.append((name,service))
-            tvlist = sorted(tvlist, key=lambda channel: channel[0]) # sort by name
-            with open(GSBQ2, "w") as f:
-                f.write("#NAME GreekStreamTV\n")
-                for (name, service) in tvlist:
-                    f.write(service)
 except Exception as err:
             print("[GreekStreamTV] Exception: %s" % err)                    
